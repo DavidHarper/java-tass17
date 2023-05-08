30a68bc125b64fe604b4efef8ac7779759337ca0
David Harper <david@obliquity.net>
2023-05-08 07:14:15 +0100

    Add methods to evaluate the series.

--- src/main/java/com/obliquity/astronomy/tass17/TASSElementSeries.java
+++ src/main/java/com/obliquity/astronomy/tass17/TASSElementSeries.java
@@ -37,6 +37,60 @@ public class TASSElementSeries {
 		return nCriticalTerms;
 	}
 	
+	public double calculateCriticalTermsInSine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = 0; i < nCriticalTerms; i++)
+			value += periodicTerms[i].getAmplitude() * Math.sin(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+	
+	public double calculateCriticalTermsInCosine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = 0; i < nCriticalTerms; i++)
+			value += periodicTerms[i].getAmplitude() * Math.cos(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+	
+	public double calculateShortPeriodTermsInSine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = nCriticalTerms; i < periodicTerms.length; i++)
+			value += periodicTerms[i].getAmplitude() * Math.sin(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+	
+	public double calculateShortPeriodTermsInCosine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = nCriticalTerms; i < periodicTerms.length; i++)
+			value += periodicTerms[i].getAmplitude() * Math.cos(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+	
+	public double calculateAllTermsInSine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = 0; i < periodicTerms.length; i++)
+			value += periodicTerms[i].getAmplitude() * Math.sin(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+	
+	public double calculateAllTermsInCosine(double t, double[] longPeriodLongitudes) {
+		double value = 0.0;
+		
+		for (int i = 0; i < periodicTerms.length; i++)
+			value += periodicTerms[i].getAmplitude() * Math.cos(periodicTerms[i].getArgument(t, longPeriodLongitudes));
+		
+		return value;
+	}
+
 	public String toString() {
 		return getClass().getName() + "[ constantTerm = " + constantTerm
 				+ ", secularRate = " + secularRate
