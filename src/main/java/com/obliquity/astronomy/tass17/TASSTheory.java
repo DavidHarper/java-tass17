56611637ec3afa38da33a02cbdc84ef64c397c97
David Harper <david@obliquity.net>
2023-05-20 09:03:05 +0100

    Fix mean motion calculation.

--- src/main/java/com/obliquity/astronomy/tass17/TASSTheory.java
+++ src/main/java/com/obliquity/astronomy/tass17/TASSTheory.java
@@ -105,7 +105,7 @@ public class TASSTheory {
 		if (position == null || position.length != 3)
 			position = new double[3];
 		
-		double am0 = TASSConstants.MEAN_MOTIONS[iSat]*(1.0 + elements.meanMotionAdjustment);
+		double am0 = 365.25 * TASSConstants.MEAN_MOTIONS[iSat] * (1.0 + elements.meanMotionAdjustment);
 		double rmu = TASSConstants.GK1 * (1.0 + TASSConstants.MASSES[iSat]);
 		double dga = pow(rmu/(am0 * am0), ONE_THIRD);
 		
