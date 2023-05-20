4dd13e6830694ee2a9f66d9b1dc628eaf008a825
David Harper <david@obliquity.net>
2023-05-20 06:41:52 +0100

    Add licence/copyright notice.

--- src/main/java/com/obliquity/astronomy/tass17/TASSElementSeries.java
+++ src/main/java/com/obliquity/astronomy/tass17/TASSElementSeries.java
@@ -1,5 +1,23 @@
 package com.obliquity.astronomy.tass17;
 
+/*
+ *  java-tass17: a Java implementation of the TASS 1.7 model of the orbits of the major satellites of Saturn
+ *  Copyright (C) 2023 David Harper at obliquity.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 public class TASSElementSeries {
 	private double constantTerm = 0.0, secularRate = 0.0;
 	private TASSPeriodicTerm[] periodicTerms = null;
